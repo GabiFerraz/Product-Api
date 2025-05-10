8963a738972976371cdae5fe0dcb170e814f5d81
Gabi Ferraz <ferrazmgabi@gmail.com>
2025-05-10 13:40:14 -0300

    self review and add dockerfile

--- src/main/java/com/api/product/core/usecase/DeleteProduct.java
+++ src/main/java/com/api/product/core/usecase/DeleteProduct.java
@@ -4,6 +4,7 @@ import com.api.product.core.gateway.ProductGateway;
 import com.api.product.core.usecase.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -11,6 +12,7 @@ public class DeleteProduct {
 
   private final ProductGateway productGateway;
 
+  @Transactional
   public void execute(final String sku) {
     this.productGateway.findBySku(sku).orElseThrow(() -> new ProductNotFoundException(sku));
     // TODO: verificar se tem estoque, se tiver, não deixar deletar
